b0e44853e32695ffe1496e11dd85ffbe3af6d9bd
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-04-11 00:25:48 -0400

    can't log out

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ var myApp = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'firebase'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
---
+++ app/authentication.js
@@ -0,0 +1,57 @@
+myApp.factory('Authentication', ['$rootScope', '$location', '$firebaseAuth', '$firebaseObject', function($rootScope, $location, $firebaseAuth, $firebaseObject){
+  var ref = firebase.database().ref();
+  var auth = $firebaseAuth();
+
+  auth.$onAuthStateChanged(function(authUser) {
+  	if(authUser) {
+  	  var userRef = ref.child('users').child(authUser.uid);
+  	  var userObj = $firebaseObject(userRef);
+  	  $rootScope.currentUser = userObj;
+  	} else {
+  	  $rootScope.currentUser = '';
+  	}
+  });
+
+  return {
+  	login: function(user) {
+      auth.$signInWithEmailAndPassword(
+        user.email,
+        user.password
+      ).then(function(user) {
+      	$location.path('#!/view1/success');
+      }).catch(function(error) {
+        $rootScope.message = error.message;
+      });
+  	},
+  	register: function(user) {
+   //    if (!$rootScope.user.firstname || $rootScope.user.firstname === "" ||
+  	// 	!$rootScope.user.lastname || $rootScope.user.lastname === "" ||
+  	// 	!$rootScope.user.email || $rootScope.user.firstname === "" ||
+  	// 	!$rootScope.user.password || $rootScope.user.password === "") {
+  	//   return;
+  	// }
+
+  	auth.$createUserWithEmailAndPassword(
+  	  user.email,
+  	  user.password
+  	).then(function(regUser) {
+  	  var regRef = ref.child('users')
+  	  .child(regUser.uid).set({
+  	  	date: firebase.database.ServerValue.TIMESTAMP,
+  	  	regUser: regUser.uid,
+  	  	firstname: user.firstname,
+  	  	lastname: user.lastname,
+  	  	email: user.email
+  	  });
+  	  $rootScope.message = "Welcome " + user.firstname;
+  	}).catch(function(error) {
+  	  $rootScope.message = error.message;
+  	});
+  	},
+  	logout: function() {
+  	  return auth.$signOut();
+  	}
+  };
+
+
+}])
